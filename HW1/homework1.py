20f52ca3db87572715b96e687fac8524f076a342
mahoep <mhoeper3234@gmail.com>
2018-05-01 18:11:00 -0700

    functions

--- HW1/homework1.py
+++ HW1/homework1.py
@@ -8,45 +8,57 @@ import time, csv, math, numpy as np
 print("")
 print("....Modules Imported....")
 
-finalScore = []
-hwheader = []
-hwheader_num = []
-labheader = []
-labheader_num = []
-lab_score_raw = []
-# convert to numpy array later
-hw_score_raw = np.array([], dtype='f')
 
-filename = 'grades.csv'
-with open(filename, 'r') as fp:
-    time.sleep(0.25)
-    print("....Importing data from", filename, "....")
-    read_obj = csv.reader(fp)
-    count = 0
-
-    for row in read_obj:
-        for x in range(len(row)):
-            if "Final Score" in row[x]:
-                try:
-                    finalScore.append(float(row[x]))
-                except ValueError:
-                    finalScore.append('null')
-            if "Homework" in row[x] and "Score" not in row[x]:
-                hwheader.append(row[x][0:len("Homework #")])
-                hwheader_num.append(x)
-            if "Lab" in row[x] and "Quiz" not in row[x] and "Score" not in row[x]:
-                labheader.append(row[x][0:len("Lab #")])
-                labheader_num.append(x)
-
-        for j in range(0, len(hwheader_num)):
-            hw_score_raw = np.append(hw_score_raw, row[hwheader_num[j]])
-            # http://akuederle.com/create-numpy-array-with-for-loop"
-
-        for j in range(0, len(labheader_num)):
-            lab_score_raw = np.append(lab_score_raw, row[labheader_num[j]])
+def read_in_csv(filename):
 
 
+    with open(filename, 'r') as fp:
+        time.sleep(0.25)
+        print("....Importing data from", filename, "....")
+        read_obj = csv.reader(fp)
+
+        headers = []
+        count = 0
+        for row in read_obj:
+            if count == 0:
+                headers.append(row)
 
 
+def read_in_np(filename):
 
 
+    data = np.genfromtxt(filename, delimiter=',')
+    #https: // stackoverflow.com / questions / 3518778 / how - to - read - csv - into - record - array - in -numpy
+    return data
+
+def read_in_csv_alt(file):
+    with open(file, 'r') as f:
+        data_raw = csv.reader(f,
+                               delimiter=',',
+                               quotechar='"')
+        data = [data for data in data_raw]
+    return data
+
+def timing():
+
+
+    filename = 'grades.csv'
+    start = time.time()
+    data = read_in_csv(filename)
+    end = time.time() - start
+    print(end)
+
+    start = time.time()
+    data = read_in_np(filename)
+    end = time.time() - start
+    print(end)
+
+    start = time.time()
+    data = read_in_csv_alt(filename)
+    end = time.time() - start
+    print(end)
+
+
+if __name__ == '__main__':
+
+    timing()
