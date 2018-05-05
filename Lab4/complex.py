93c4f8ff2b41287bf16e2a9016a28ba9b48f6555
mahoep <mahoep@users.noreply.github.com>
2018-05-05 01:05:36 -0700

    fixed small bug

--- Lab4/complex.py
+++ Lab4/complex.py
@@ -3,25 +3,23 @@
     @author Matthew Hoeper
 '''
 
+
 class Complex:
     def __init__(self, re=0, im=0):
         self.re = re
         self.im = im
 
         if im < 0:
-            r = '{} - {}i'.format(re, abs(im))
+            r = '{} - {}i'.format(self.re, abs(self.im))
         else:
-            r = '{} + {}i'.format(re, im)
+            r = '{} + {}i'.format(self.re, self.im)
         return print(r)
 
     def __str__(self, re=0, im=0):
-        self.re = re
-        self.im = im
-        
-        if im < 0:
-            r = '({} - {}i)'.format(re, abs(im))
+        if self.im < 0:
+            r = '({} - {}i)'.format(self.re, abs(self.im))
         else:
-            r = '({} + {}i)'.format(re, im)
+            r = '({} + {}i)'.format(self.re, self.im)
         return r
 
 if __name__ == '__main__':
