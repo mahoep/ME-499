4b548b2fc8c7f26e0f0888d41afbe7a6e04fc733
mahoep <mhoeper3234@gmail.com>
2018-06-04 15:58:03 -0700

    lab7

--- Lab7/optimize.py
+++ Lab7/optimize.py
@@ -11,32 +11,50 @@ def optimize_step(f, bounds, n):
     :param f: f is the function
     :param bounds: bounds is a tuple specifying the lower (inclusive) and upper (exclusive) bounds
     :param n: n is the number of steps
-    :return: returning the x value of the largest one
+    :return: returning the x value of the largest x value where max occurs
     '''
 
     low = bounds[0]
     up = bounds[1]
-    x = np.linspace(low, up, 10000)
+    x = np.linspace(low, up, n)
     y = f(x)
-    y_chuncks = np.array_split(y, n)
-    max_lst = []
-    idx_lst = []
-    for i in y_chuncks:
-        max_lst.append(max(i))
-        idx_lst.append(np.argmax(i))
-    max_chunk = np.argmax(max_lst)
-    max_idx = idx_lst[max_chunk]
-    x_max = len(y_chuncks[0])*(max_chunk) + max_idx
+    x_idx = np.argmax(y)
 
-    return x[x_max]
+    return x[x_idx]
+
+
+def optimize_random(f, bounds, n):
+    '''
+    finds the largest value of a function, between a set of bounds
+    :param f: f is the function
+    :param bounds: bounds is a tuple specifying the lower (inclusive) and upper (exclusive) bounds
+    :param n: n random samples between the bounds
+    :return: returning the x value of the largest x value where max occurs
+    '''
+
+    max = 0
+    for i in range(n):
+        low = bounds[0]
+        up = bounds[1]
+        num = np.random.random_integers(low, up) + np.random.random()
+        y = func(num)
+        if y > max:
+            max = num
+        else:
+            pass
+
+    return max
 
 
 def func(x):
     return -x**2
 
+
 if __name__ == '__main__':
 
-    bounds = (-10, 10)
-    n = 25
-    new = optimize_step(func, bounds, n)
-    print(new)
+    bounds = (-5, 5)
+    n = 2500
+    # step = optimize_step(func, bounds, n)
+    # print(new)
+    rand = optimize_random(func, bounds, n)
+    print(rand)
