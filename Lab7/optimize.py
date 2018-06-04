b0a1d793483e8e35d071efdbd8bf3c1bca84be78
mahoep <mahoep@users.noreply.github.com>
2018-06-04 13:35:43 -0700

    lab7

---
+++ Lab7/optimize.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python3
+'''
+    @author Matthew Hoeper
+'''
+
+import numpy as np
+
+def optimize_step(f, bounds, n):
+    '''
+    finds the largest value of a function, between a set of bounds
+    :param f: f is the function
+    :param bounds: bounds is a tuple specifying the lower (inclusive) and upper (exclusive) bounds
+    :param n: n is the number of steps
+    :return: returning the x value of the largest one
+    '''
+
+
+if __name__ == '__main__':
+    f = 
